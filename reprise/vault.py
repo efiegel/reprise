8a5feeb89a90ee398931a5710fef24267c112add
efiegel <efiegel01@gmail.com>
2024-09-28 12:30:42 -0500

    move vault dir fetching up a level

--- reprise/vault.py
+++ reprise/vault.py
@@ -1,11 +1,9 @@
-import os
-
 from reprise.motif import Motif
 
 
 class Vault:
-    def __init__(self) -> None:
-        self.directory = os.path.expanduser(os.getenv("VAULT_DIRECTORY"))
+    def __init__(self, directory) -> None:
+        self.directory = directory
 
     def add_motif(self, content: str) -> Motif:
         motif = Motif(content)
--- tests/integration/test_vault.py
+++ tests/integration/test_vault.py
@@ -2,11 +2,10 @@ from reprise.vault import Vault
 
 
 class TestVault:
-    def test_add_motif(self, tmp_path, monkeypatch):
-        monkeypatch.setenv("VAULT_DIRECTORY", tmp_path)
-        content = "Hello, World!"
-        vault = Vault()
-        motif = vault.add_motif(content)
+    def test_add_motif(self, tmp_path):
+        vault = Vault(tmp_path)
 
+        content = "Hello, World!"
+        motif = vault.add_motif(content)
         with open(f"{tmp_path}/{motif.uuid}.md") as file:
             assert file.read() == content
