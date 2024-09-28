ad5a00638dd5fd5abf042dd2aef0a83f1605e41f
efiegel <efiegel01@gmail.com>
2024-09-28 12:24:37 -0500

    move motif persistence dir up a level

--- reprise/motif.py
+++ reprise/motif.py
@@ -1,14 +1,7 @@
-import os
 from uuid import uuid4
-
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Motif:
-    PERSISTENCE_DIR = f"{os.path.expanduser(os.getenv('VAULT_DIRECTORY'))}/.motifs"
-
     def __init__(self, content: str):
         self.content = content
         self.uuid = uuid4()
@@ -16,8 +9,8 @@ class Motif:
     def _format_obsidian_file(self):
         return self.content
 
-    def save(self):
-        file_path = f"{self.PERSISTENCE_DIR}/{self.uuid}.md"
+    def save(self, directory: str):
+        file_path = f"{directory}/{self.uuid}.md"
         with open(file_path, "w") as file:
             file_content = self._format_obsidian_file()
             file.write(file_content)
--- tests/unit/test_motif.py
+++ tests/unit/test_motif.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from reprise.motif import Motif
 
 
@@ -7,8 +5,7 @@ class TestMotif:
     def test_create_motif(self, tmp_path):
         content = "Hello, World!"
         motif = Motif(content)
-        with patch.object(motif, "PERSISTENCE_DIR", tmp_path):
-            motif.save()
+        motif.save(tmp_path)
 
         with open(f"{tmp_path}/{motif.uuid}.md") as file:
             assert file.read() == content
