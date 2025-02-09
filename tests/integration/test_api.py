00b3863c6b57ef52d56513dbd0b5cbc23687690c
efiegel <efiegel01@gmail.com>
2025-02-09 16:00:35 -0600

    use factory in api tests too

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,26 +1,25 @@
-from datetime import datetime
-from uuid import uuid4
-
+import pytest
 from flask import json
 
 from reprise.db import Motif, database_session
+from reprise.factories import motif_factory
 
 
 class TestAPI:
-    def test_get_motifs(self, session, client):
-        motif = Motif(content="Test content", citation="Test citation")
-        session.add(motif)
-        session.commit()
+    @pytest.fixture
+    def motif(self, session):
+        return motif_factory(session=session).create()
 
+    def test_get_motifs(self, client, motif):
         response = client.get("/motifs")
         data = json.loads(response.data)
 
         assert response.status_code == 200
         assert len(data) == 1
-        assert data[0]["content"] == "Test content"
-        assert data[0]["citation"] == "Test citation"
+        assert data[0]["content"] == motif.content
+        assert data[0]["citation"] == motif.citation
 
-    def test_add_motif(self, session, client):
+    def test_add_motif(self, client):
         response = client.post(
             "/motifs",
             data=json.dumps({"content": "New motif content"}),
@@ -31,19 +30,11 @@ class TestAPI:
         assert response.status_code == 200
         assert data["content"] == "New motif content"
 
-        motif = session.query(Motif).filter_by(uuid=data["uuid"]).one_or_none()
-        assert motif.content == "New motif content"
+        with database_session() as session:
+            motif = session.query(Motif).filter_by(uuid=data["uuid"]).one_or_none()
+            assert motif.content == "New motif content"
 
-    def test_update_motif(self, session, client):
-        motif = Motif(
-            uuid=str(uuid4()),
-            content="Old content",
-            citation="Old citation",
-            created_at=datetime.now(),
-        )
-        session.add(motif)
-        session.commit()
-
+    def test_update_motif(self, client, motif):
         response = client.put(
             f"/motifs/{motif.uuid}",
             data=json.dumps({"content": "Updated content"}),
@@ -54,24 +45,15 @@ class TestAPI:
         assert response.status_code == 200
         assert data["content"] == "Updated content"
 
-        with database_session() as new_session:
-            motif = new_session.query(Motif).filter_by(uuid=motif.uuid).one_or_none()
+        with database_session() as session:
+            motif = session.query(Motif).filter_by(uuid=motif.uuid).one_or_none()
             assert motif.content == "Updated content"
 
-    def test_delete_motif(self, session, client):
-        motif = Motif(
-            uuid=str(uuid4()),
-            content="Content to delete",
-            citation="Citation to delete",
-            created_at=datetime.now(),
-        )
-        session.add(motif)
-        session.commit()
-
+    def test_delete_motif(self, client, motif):
         response = client.delete(f"/motifs/{motif.uuid}")
 
         assert response.status_code == 200
         assert json.loads(response.data)["message"] == "Motif deleted"
 
-        with database_session() as new_session:
-            assert len(new_session.query(Motif).filter_by(uuid=motif.uuid).all()) == 0
+        with database_session() as session:
+            assert len(session.query(Motif).filter_by(uuid=motif.uuid).all()) == 0
