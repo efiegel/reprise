95639d217e8d40b77b7e7bc80f34bcc3585a46fb
efiegel <efiegel01@gmail.com>
2025-02-09 15:55:06 -0600

    use motif factory for repository tests

--- tests/unit/test_repository.py
+++ tests/unit/test_repository.py
@@ -1,9 +1,19 @@
+import pytest
+
+from reprise.factories import motif_factory
 from reprise.repository import MotifRepository
 
 
 class TestRepository:
-    def test_add_motif(self, session):
-        repository = MotifRepository(session)
+    @pytest.fixture
+    def repository(self, session):
+        return MotifRepository(session)
+
+    @pytest.fixture
+    def motif(self, session):
+        return motif_factory(session=session).create()
+
+    def test_add_motif(self, repository):
         content, citation = "Hello, World!", "my-citation"
         motif = repository.add_motif("Hello, World!", "my-citation")
         assert motif.uuid is not None
@@ -11,28 +21,18 @@ class TestRepository:
         assert motif.content == content
         assert motif.citation == citation
 
-    def test_get_motif(self, session):
-        repository = MotifRepository(session)
-        motif = repository.add_motif("Hello, World!", "my-citation")
+    def test_get_motif(self, repository, motif):
         assert repository.get_motif(motif.uuid) == motif
 
-    def test_get_motifs(self, session):
-        repository = MotifRepository(session)
-        repository.add_motif("Hello, World!", "my-citation")
-        repository.add_motif("Hello again, World!", "my-citation")
+    def test_get_motifs(self, session, repository):
+        motif_factory(session=session).create_batch(2)
         assert len(repository.get_motifs()) == 2
 
-    def test_update_motif_content(self, session):
-        repository = MotifRepository(session)
-        motif = repository.add_motif("Hello, World!", "my-citation")
-
+    def test_update_motif_content(self, repository, motif):
         new_content = "Hello, Universe!"
         updated_motif = repository.update_motif_content(motif.uuid, new_content)
         assert updated_motif.content == new_content
 
-    def test_delete_motif(self, session):
-        repository = MotifRepository(session)
-        motif = repository.add_motif("Hello, World!", "my-citation")
-
+    def test_delete_motif(self, repository, motif):
         repository.delete_motif(motif.uuid)
         assert repository.get_motif(motif.uuid) is None
