541a8ef0bc916825d1ba76576e06e02883280acf
efiegel <efiegel01@gmail.com>
2025-02-09 11:34:53 -0600

    enable :memory: db for tests only, otherwise use reprise.db

--- reprise/db.py
+++ reprise/db.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import contextmanager
 from datetime import datetime
 from uuid import uuid4
@@ -5,8 +6,10 @@ from uuid import uuid4
 from sqlalchemy import Column, DateTime, String, Text, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+database = os.getenv("DATABASE_URL", "sqlite:///reprise.db")
+engine = create_engine(database, echo=False)
+
 Base = declarative_base()
-engine = create_engine("sqlite:///:memory:", echo=False)
 Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(bind=engine)
