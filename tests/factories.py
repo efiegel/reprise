737ccc8b1a0f083d3a6f6e892eb855c06c013c26
efiegel <efiegel01@gmail.com>
2025-03-02 20:05:42 -0600

    add citation factory

--- tests/factories.py
+++ tests/factories.py
@@ -1,7 +1,7 @@
 import factory
 from factory.alchemy import SQLAlchemyModelFactory
 
-from reprise.db import Motif
+from reprise.db import Citation, Motif
 
 
 def motif_factory(session):
@@ -15,4 +15,20 @@ def motif_factory(session):
         content = factory.Faker("text")
         created_at = factory.Faker("date_time")
 
+        citation = factory.SubFactory(citation_factory(session))
+
     return _MotifFactory
+
+
+def citation_factory(session):
+    class _CitationFactory(SQLAlchemyModelFactory):
+        class Meta:
+            model = Citation
+            sqlalchemy_session = session
+            sqlalchemy_session_persistence = "commit"
+
+        uuid = factory.Faker("uuid4")
+        title = factory.Faker("text")
+        created_at = factory.Faker("date_time")
+
+    return _CitationFactory
