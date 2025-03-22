192683173d7a88dd98dcd77d17df12ad16db8967
efiegel <efiegel01@gmail.com>
2025-03-22 09:27:44 -0500

    dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,14 @@ import TabsContainer from "./components/TabsContainer";
 import MotifsTab from "./components/MotifsTab";
 import CitationsTab from "./components/CitationsTab";
 import ReprisalsTab from "./components/ReprisalsTab";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   const tabs = [
@@ -13,10 +21,13 @@ function App() {
   ];
 
   return (
-    <Container>
-      <h1>Reprise</h1>
-      <TabsContainer tabs={tabs} />
-    </Container>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Reprise</h1>
+        <TabsContainer tabs={tabs} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
